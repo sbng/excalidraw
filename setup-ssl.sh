ca080b29d45e868d8bcde2dd42ee70f6bce6a3b7
sbng <sbng@cisco.com>
2020-05-27 21:08:40 +0800

    Check for exsistence of openssl

--- setup-ssl.sh
+++ setup-ssl.sh
@@ -6,6 +6,11 @@
 # Distributed under terms of the MIT license.
 #
 
+if ! [ -x "$(command -v openssl)" ]; then
+  echo '[Error] openssl is not installed. Please install and rerun the script' >&2
+  exit 1
+fi
+
 echo "[INFO] Create SSL certs and keys, please enter the information:"
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./ssl/private/nginx-selfsigned.key -out ./ssl/certs/nginx-selfsigned.crt
 echo "[INFO] Create Diffie-Hellman group, may require a few minutes...."
