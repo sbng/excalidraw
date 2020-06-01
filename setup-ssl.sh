b1743cf04eeabf3df83e882b8260cd91152c599e
sbng <sbng@cisco.com>
2020-06-02 07:04:05 +0800

    mkdir for certifcates

--- setup-ssl.sh
+++ setup-ssl.sh
@@ -11,6 +11,9 @@ if ! [ -x "$(command -v openssl)" ]; then
   exit 1
 fi
 
+mkdir -p ./ssl/certs
+mkdir -p  ./ssl/private
+
 echo "[INFO] Create SSL certs and keys, please enter the information:"
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./ssl/private/nginx-selfsigned.key -out ./ssl/certs/nginx-selfsigned.crt
 echo "[INFO] Create Diffie-Hellman group, may require a few minutes...."
